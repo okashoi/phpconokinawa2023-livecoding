b72c48d8652e14c0d478be5169f0144552da7079
Okada Shohei <okashoicircus0@gmail.com>
2023-08-23 00:46:35 +0900

    テストコードに DataProvider を使用

--- tests/TriangleTest.php
+++ tests/TriangleTest.php
@@ -4,31 +4,25 @@ declare(strict_types=1);
 
 use Okashoi\MyersTriangle\Triangle;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\{DataProvider, Test};
 
 class TriangleTest extends TestCase
 {
     #[Test]
-    public function 三辺の長さが等しければ正三角形であること(): void
+    #[DataProvider('getTypeDataProvider')]
+    public function getType($a, $b, $c, $expected): void
     {
-        $triangle = new Triangle(1, 1, 1);
+        $triangle = new Triangle($a, $b, $c);
 
-        $this->assertSame('正三角形', $triangle->getType());
+        $this->assertSame($expected, $triangle->getType());
     }
 
-    #[Test]
-    public function 二辺の長さが等しければ二等辺三角形であること(): void
+    public static function getTypeDataProvider()
     {
-        $triangle = new Triangle(1, 2, 2);
-
-        $this->assertSame('二等辺三角形', $triangle->getType());
-    }
-
-    #[Test]
-    public function 三辺の長さがすべて異なれば不等辺三角形であること(): void
-    {
-        $triangle = new Triangle(2, 3, 4);
-
-        $this->assertSame('不等辺三角形', $triangle->getType());
+        return [
+            '三辺の長さが等しければ正三角形であること' => [1, 1, 1, '正三角形'],
+            '二辺の長さが等しければ二等辺三角形であること' => [1, 2, 2, '二等辺三角形'],
+            '三辺の長さがすべて異なれば不等辺三角形であること' => [2, 3, 4, '不等辺三角形'],
+        ];
     }
 }
