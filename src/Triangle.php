089766ace2ad3870a99b7fed54401ddaac335ced
Okada Shohei <okashoicircus0@gmail.com>
2023-08-23 01:09:49 +0900

    不正な入力値の判定ロジックをデバッグ

--- src/Triangle.php
+++ src/Triangle.php
@@ -8,11 +8,19 @@ use InvalidArgumentException;
 
 class Triangle
 {
+    private $a; // 辺 A の長さ
+    private $b; // 辺 B の長さ
+    private $c; // 辺 C の長さ
+
     public function __construct(
-        private $a, // 辺 A の長さ
-        private $b, // 辺 B の長さ
-        private $c, // 辺 C の長さ
+        $a,
+        $b,
+        $c,
     ) {
+        $a = filter_var($a, FILTER_VALIDATE_INT);
+        $b = filter_var($b, FILTER_VALIDATE_INT);
+        $c = filter_var($c, FILTER_VALIDATE_INT);
+
         if (!is_int($a) || !is_int($b) || !is_int($c)) {
             throw new InvalidArgumentException('三角形の3辺の長さは正の整数でなければなりません。');
         }
@@ -20,6 +28,10 @@ class Triangle
         if ($a <= 0 || $b <= 0 || $c <= 0) {
             throw new InvalidArgumentException('三角形の3辺の長さは正の整数でなければなりません。');
         }
+
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
     }
 
     /**
